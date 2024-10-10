4c86887445059b68617b2855ca5993a2cb53d632
Mathias Appelmans <math.appelmans@gmail.com>
2024-10-10 15:55:47 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-        {
-            protocol: "https",
-            hostname: "images.pexels.com",
-        }
-    ]
-},
   experimental: {
     reactCompiler: true,
     ppr: 'incremental'
