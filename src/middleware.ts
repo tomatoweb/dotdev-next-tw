a4859096f1ab45aa5f5810919c95a9b43324332a
Mathias Appelmans <math.appelmans@gmail.com>
2024-11-08 17:26:33 +0100

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   // Clone the request headers so that we don't modify the original headers object
-  const requestHeaders = new Headers(request.headers);  
+  const requestHeaders = new Headers(request.headers);
 
   // Return a new request object with the updated headers using NextResponse.next()
   return NextResponse.next({
