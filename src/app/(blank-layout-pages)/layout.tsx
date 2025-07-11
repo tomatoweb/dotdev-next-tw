8efccdeab4dc3fff857cff27e26940fb66382cc0
Mathias Appelmans <math.appelmans@gmail.com>
2025-07-11 16:45:55 +0200

    added openStreetMap

--- src/app/(blank-layout-pages)/layout.tsx
+++ src/app/(blank-layout-pages)/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
         <html lang="en">
         <body className={roboto.className}>
-                {children}
+            <main>{children}</main>
         </body>
         </html>
   );
