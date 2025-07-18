c6b227a892622497c21a1cfe371d2fd3035b6287
Mathias Appelmans <math.appelmans@gmail.com>
2025-07-18 17:13:08 +0200

    enhanced navbar style

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,25 +6,22 @@ import { slides } from "@/lib/slides";
 const Navbar = () => {
 
     return (
-    <>
         <div className="flex justify-between">
             <Link className="-mt-6 mr-2 -ml-6" href="/">
                 <Image alt="" src="/mathias.png" width={100} height={100} />
             </Link>
-           
             <div className="grid grid-cols-4 md:grid-cols-8 gap-2 lg:gap-6" style={{zIndex: 10}}>
-
             {slides.map((slide) => (
                 <Link key={slide.id} className="flex flex-col items-center" href={slide.url} target="_blank">
-                <div style={{backgroundImage: `url(${slide.imgsmall})`}} className={`h-16 w-16 sm:h-24 sm:w-24 md:h-16 md:w-16 lg:h-24 lg:w-24 bg-cover bg-center bg-no-repeat size-18 rounded-full hover:scale-125 transition-all duration-300 shadow-xl`}></div>
+                <div style={{backgroundImage: `url(${slide.imgsmall})`}} className={`h-14 w-14 sm:h-24 sm:w-24 md:h-16 md:w-16 lg:h-24 lg:w-24 bg-cover bg-center bg-no-repeat size-18 rounded-full hover:scale-125 transition-all duration-300 shadow-xl`}></div>
                 <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800 hidden">
                     slide.title
                 </div>
             </Link>
-            ))}                
+            ))}
             </div>
-            <Link className="bg-[#238636] text-white rounded-lg h-fit z-50 ml-2 p-2"
-                href="https://github.com/tomatoweb/dotdev-next-tw" target="_blank">                
+            <Link className="bg-[#238636] text-white rounded-lg h-fit z-50 ml-2 -mr-1 p-2 -mt-6"
+                href="https://github.com/tomatoweb/dotdev-next-tw" target="_blank">
                 <span className="ml-2 mr-1 font-thin tracking-normal text-xs hidden">
                     My GitHub
                 </span>
@@ -34,96 +31,6 @@ const Navbar = () => {
                 </div>
             </Link>
         </div>
-
-
-        {/* <div className="navbar flex justify-between items-center relative" style={{zIndex: 1000}}>
-            <div>
-                <div className="flex items-center">
-                    <Link className="lg:-mt-20 md:-ml-9 w-20 lg:w-15 xl:flex text-2xl md:text-3xl font-bold tracking-tighter" href="/">
-                        <Image alt="" src="/mathias.png" width={100} height={100} />
-                    </Link>
-                </div>
-            </div>
-            <div>
-                <div className="text-md items-center justify-end hidden lg:flex">
-                    <div className="flex gap-4 text-gray-700">
-                        <div>
-                            <Link className="flex flex-col items-center" href="https://mathiasappelmans.be/geo" target="_blank">
-                                <Image className="rounded-2xl hover:scale-125 transition-all duration-300 w-20 lg:w-32 max-h-[5.5rem]" alt="" src="/geo.png" width={100} height={100} />
-                                <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800">
-                                    React Leaflet
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="flex flex-col items-center" href="https://symfony.dotdev.be" target="_blank">
-                                <Image className="rounded-2xl hover:scale-125 transition-all duration-300  w-20 lg:w-32  max-h-[5.5rem]" alt="" src="/symfony-app-small.png" width={100} height={0} />
-                                <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800">
-                                eShop Symfony
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="flex flex-col items-center" href="https://social.dotdev.be" target="_blank">
-                                <Image className="rounded-2xl hover:scale-125 transition-all duration-300  w-20 lg:w-32  max-h-[5.5rem]" alt="" src="/social-react-small.png" width={100} height={0} />
-                                <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800">
-                                Social React
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="flex flex-col items-center" href="https://dashboard.dotdev.be" target="_blank">
-                                <Image className="rounded-2xl hover:scale-125 transition-all duration-300  w-20 lg:w-32  max-h-[5.5rem]" alt="" src="/dashboard-next-small.png" width={100} height={0} />
-                                <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800">
-                                Dashboard Nextjs
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="flex flex-col items-center" href="https://e-shop.dotdev.be" target="_blank">
-                                <Image className="rounded-2xl hover:scale-125 transition-all duration-300  w-20 lg:w-32  max-h-[5.5rem]" alt="" src="/eshop-next-small.png" width={100} height={0} />
-                                <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800">
-                                eShop React
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="flex flex-col items-center" href="https://doc.dotdev.be" target="_blank">
-                                <Image className="rounded-2xl hover:scale-125 transition-all duration-300  w-20 lg:w-32  max-h-[5.5rem]" alt="" src="/dotdev-next-small.png" width={100} height={0} />
-                                <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800">
-                                Admin Nextjs
-                                </div>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link className="flex flex-col items-center" href="https://laravel.dotdev.be" target="_blank">
-                                <Image className="rounded-2xl hover:scale-125 transition-all duration-300  w-20 lg:w-32  max-h-[5.5rem]" alt="" src="/eshop-laravel-small.png" width={100} height={0} />
-                                <div className="text-sm text-center font-thin tracking-normal w-[100px] leading-normal mt-2 text-blue-800">
-                                eShop Laravel
-                                </div>
-                            </Link>
-                        </div>
-                        {/* <div>
-                            <a href="/Mathias_Appelmans_2025.pdf" target="_blank" className="flex">
-                                <button className="text-sm w-[80px] leading-normal mt-2 text-blue-800">Curriculum Vitæ</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="lg:ml-1 lg:-mt-24 w-20 ml-[1%]">
-                <Link
-                    href="https://github.com/tomatoweb/dotdev-next-tw"
-                    target="_blank"
-                    className="flex axil-btn btn-fill-primary h-10 justify-center mr-[3px]">
-                    <span className="ml-2 mr-1 font-thin tracking-normal text-xs">
-                        My GitHub
-                    </span>
-                    <Github className="-mr-1" />
-                </Link>
-            </div>
-        </div> */}
-        </>
     )
 }
 
